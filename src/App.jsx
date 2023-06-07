16412b8bac773d5f4fe4427fe030c8c281a207c9
Your Name <arashlatifioo44@gmail.com>
2023-06-07 15:48:27 +0330

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
     <Hero/>
     <Cards/>
     <Banner />
+    <App />
     </>
    );
 }
---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h1>
+        © Copyright 2023 Nextjs Starter. Powered with ♥ by CreativeDesignsGuru
+      </h1>
+    </footer>
+  );
+};
+
+export default Footer;
